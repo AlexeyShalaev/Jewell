fb4f3a06a7d00aecba0b9cd3b768a8eb9ba78716
AlexeyShalaev <shalaevad.alexey@yandex.ru>
2023-04-11 20:00:52 +0300

    fixed terminal.py for Ubuntu

--- ManagementSystem/ext/terminal.py
+++ ManagementSystem/ext/terminal.py
@@ -4,21 +4,21 @@ bot_service = 'jms_bot.service'
 
 
 def get_telegram_bot_status():
-    cmd = f'systemctl status {bot_service}'
+    cmd = f'/usr/bin/systemctl status {bot_service}'
     status = os.system(cmd)
     return status == 0
 
 
 def stop_telegram_bot():
-    cmd = f'systemctl stop {bot_service}'
+    cmd = f'/usr/bin/systemctl stop {bot_service}'
     os.system(cmd)
 
 
 def start_telegram_bot():
-    cmd = f'systemctl start {bot_service}'
+    cmd = f'/usr/bin/systemctl start {bot_service}'
     os.system(cmd)
 
 
 def restart_telegram_bot():
-    cmd = f'systemctl restart {bot_service}'
+    cmd = f'/usr/bin/systemctl restart {bot_service}'
     os.system(cmd)
